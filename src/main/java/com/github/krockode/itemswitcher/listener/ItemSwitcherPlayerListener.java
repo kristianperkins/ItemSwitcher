373e8a5da40c230a11ff63d85d7886a6277adf85
krockode <khperkins@gmail.com>
2012-08-05 23:15:03 +1000

    moved block matching config into classpath

--- src/main/java/com/github/krockode/itemswitcher/listener/ItemSwitcherPlayerListener.java
+++ src/main/java/com/github/krockode/itemswitcher/listener/ItemSwitcherPlayerListener.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -20,13 +21,13 @@ import com.github.krockode.itemswitcher.util.SwitcherStatus;
 
 public class ItemSwitcherPlayerListener implements Listener {
 
+    private static final String MATCHERS_CONFIG_FILE = "/com/github/krockode/itemswitcher/matchers.yml";
     private final Logger log;
     // item in hand to enable switching
     private final String enableSwitchingRegex;
     private final List<ItemSwitcherBlockMatcher> blockMatchers;
     private final Map<String, SwitcherStatus> enabledPlayers;
 
-    @SuppressWarnings("unchecked")
     public ItemSwitcherPlayerListener(final Plugin plugin, Map<String, SwitcherStatus> enabledPlayers) {
         this.log = plugin.getLogger();
         Configuration configuration = plugin.getConfig();
@@ -36,10 +37,11 @@ public class ItemSwitcherPlayerListener implements Listener {
         enableSwitchingRegex = configuration.getString("enable_switching_regex");
         blockMatchers = new ArrayList<ItemSwitcherBlockMatcher>();
 
-        for (Object matcherNode : configuration.getList("block_matchers")) {
+        YamlConfiguration matchersConfig = YamlConfiguration.loadConfiguration(getClass().getResourceAsStream(MATCHERS_CONFIG_FILE));
+        for (Object matcherNode : matchersConfig.getList("block_matchers")) {
+            @SuppressWarnings("unchecked")
             Map<String, String> matcherMap = (Map<String, String>) matcherNode;
-            blockMatchers.add(new ItemSwitcherBlockMatcher(
-                    matcherMap.get("block_regex"), matcherMap.get("item_regex")));
+            blockMatchers.add(new ItemSwitcherBlockMatcher(matcherMap.get("block_regex"), matcherMap.get("item_regex")));
         }
         log.info("ItemSwitcher loaded " + blockMatchers.size() + " block matching rules.");
     }
